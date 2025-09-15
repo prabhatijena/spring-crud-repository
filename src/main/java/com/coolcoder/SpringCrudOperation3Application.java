8343f015a1b28a52cad4fac222609513de4f5607
prabhatijena <jenaprabhati97@gmail.com>
2025-09-15 20:26:27 +0530

    Update SpringCrudOperation3Application.java

--- src/main/java/com/coolcoder/SpringCrudOperation3Application.java
+++ src/main/java/com/coolcoder/SpringCrudOperation3Application.java
@@ -8,6 +8,7 @@ public class SpringCrudOperation3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCrudOperation3Application.class, args);
+		String s = "Prabhati";
 	}
 
 }
